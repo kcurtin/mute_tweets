1c7936bdba1aee87865aacea74705d6fbc434cbf
Kevin Curtin <kevincurtin88@gmail.com>
2012-09-21 12:41:21 -0400

    prepare for merge

--- mute_tweets.js
+++ mute_tweets.js
@@ -1,9 +1,9 @@
 javascript:(function(){   
-    var filterTerm = prompt("Enter a term and we will hide all of the tweets that contain it!");
-    filterFunction(filterTerm);
-    function filterFunction(filterTerm) {
+    var muteTerm = prompt("Enter a term and we will hide all of the tweets that contain it!");
+    muteFunction(muteTerm);
+    function muteFunction(muteTerm) {
       $(".stream-item").each(function(){
-          if ($(this).find('p.js-tweet-text').text().toLowerCase().indexOf(filterTerm) != -1) {
+          if ($(this).find('p.js-tweet-text').text().toLowerCase().indexOf(muteTerm) != -1) {
             $(this).hide();
           }
       });
